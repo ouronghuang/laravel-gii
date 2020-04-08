3a1c43a15e9d5b12474d47112e7abd4e5bb7f84d
欧荣凰 <512574561@qq.com>
2020-04-08 03:57:38 +0000

    修复排版错误

--- resources/views/gii/create.blade.php
+++ resources/views/gii/create.blade.php
@@ -31,89 +31,87 @@
   <div id="app" v-cloak>
     <h1 class="text-center my-5">Gii</h1>
     <div>
-      <div class="row justify-content-center">
-        <div class="col-12 col-sm-6">
-          <i-form ref="formValidate" :model="formValidate" :rules="ruleValidate" label-position="top">
-            <div class="row">
-              <div class="col">
-                <form-item label="模块名">
-                  <i-input v-model="formValidate.module" placeholder="可不填" clearable></i-input>
-                </form-item>
-              </div>
-              <div class="col">
-                <form-item label="表名" prop="table">
-                  <i-input v-model="formValidate.table" placeholder="请输入表名" clearable></i-input>
-                </form-item>
-              </div>
-            </div>
-            <div>
-              <div class="row align-items-center" v-for="(v, k) in formValidate.columns" :key="k">
-                <div class="col">
-                  <form-item
-                    label="字段名"
-                    :prop="`columns.${k}.name`"
-                    :rules="{required: true, message: '请输入字段名', trigger: 'change'}"
-                  >
-                    <i-input v-model="v.name" placeholder="请输入字段名" clearable></i-input>
-                  </form-item>
-                </div>
-                <div class="col">
-                  <form-item
-                    label="类型"
-                    :prop="`columns.${k}.type`"
-                    :rules="{required: true, message: '请选择类型', trigger: 'change'}"
-                  >
-                    <i-select placeholder="请选择类型" v-model="v.type">
-                      <i-option :value="type" v-for="type in columnTypes" :key="type">
-                        @{{ type }}
-                      </i-option>
-                    </i-select>
-                  </form-item>
-                </div>
-                <div class="col-auto">
-                  <form-item label="可空">
-                    <i-switch size="large" v-model="v.nullable">
-                      <span slot="open">可空</span>
-                      <span slot="close">非空</span>
-                    </i-switch>
-                  </form-item>
-                </div>
-                <div class="col">
-                  <form-item label="默认">
-                    <i-input v-model="v.default" placeholder="可不填" clearable></i-input>
-                  </form-item>
-                </div>
-                <div class="col">
-                  <form-item label="注释">
-                    <i-input v-model="v.comment" placeholder="可不填" clearable></i-input>
-                  </form-item>
-                </div>
-                <div class="col-auto">
-                  <form-item label="操作">
-                    <poptip
-                      confirm
-                      title="确认删除？"
-                      @on-ok="delColumn(k)">
-                      <i-button type="error">
-                        删除
-                      </i-button>
-                    </poptip>
-                  </form-item>
-                </div>
-              </div>
-              <form-item>
-                <i-button type="dashed" long @click="addColumn">
-                  新增字段
-                </i-button>
+      <div class="container">
+        <i-form ref="formValidate" :model="formValidate" :rules="ruleValidate" label-position="top">
+          <div class="row">
+            <div class="col">
+              <form-item label="模块名">
+                <i-input v-model="formValidate.module" placeholder="可不填" clearable></i-input>
               </form-item>
             </div>
+            <div class="col">
+              <form-item label="表名" prop="table">
+                <i-input v-model="formValidate.table" placeholder="请输入表名" clearable></i-input>
+              </form-item>
+            </div>
+          </div>
+          <div>
+            <div class="row align-items-center" v-for="(v, k) in formValidate.columns" :key="k">
+              <div class="col">
+                <form-item
+                  label="字段名"
+                  :prop="`columns.${k}.name`"
+                  :rules="{required: true, message: '请输入字段名', trigger: 'change'}"
+                >
+                  <i-input v-model="v.name" placeholder="请输入字段名" clearable></i-input>
+                </form-item>
+              </div>
+              <div class="col">
+                <form-item
+                  label="类型"
+                  :prop="`columns.${k}.type`"
+                  :rules="{required: true, message: '请选择类型', trigger: 'change'}"
+                >
+                  <i-select placeholder="请选择类型" v-model="v.type">
+                    <i-option :value="type" v-for="type in columnTypes" :key="type">
+                      @{{ type }}
+                    </i-option>
+                  </i-select>
+                </form-item>
+              </div>
+              <div class="col-auto">
+                <form-item label="可空">
+                  <i-switch size="large" v-model="v.nullable">
+                    <span slot="open">可空</span>
+                    <span slot="close">非空</span>
+                  </i-switch>
+                </form-item>
+              </div>
+              <div class="col">
+                <form-item label="默认">
+                  <i-input v-model="v.default" placeholder="可不填" clearable></i-input>
+                </form-item>
+              </div>
+              <div class="col">
+                <form-item label="注释">
+                  <i-input v-model="v.comment" placeholder="可不填" clearable></i-input>
+                </form-item>
+              </div>
+              <div class="col-auto">
+                <form-item label="操作">
+                  <poptip
+                    confirm
+                    title="确认删除？"
+                    @on-ok="delColumn(k)">
+                    <i-button type="error">
+                      删除
+                    </i-button>
+                  </poptip>
+                </form-item>
+              </div>
+            </div>
             <form-item>
-              <i-button type="primary" long @click="handleSubmit" :loading="loading">
-                提交
+              <i-button type="dashed" long @click="addColumn">
+                新增字段
               </i-button>
             </form-item>
-          </i-form>
-        </div>
+          </div>
+          <form-item>
+            <i-button type="primary" long @click="handleSubmit" :loading="loading">
+              提交
+            </i-button>
+          </form-item>
+        </i-form>
       </div>
     </div>
     <div>
