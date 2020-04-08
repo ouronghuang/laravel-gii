d94b070f7805bdc1cbab7355d498ca2c25bcf893
欧荣凰 <512574561@qq.com>
2020-04-08 08:37:18 +0000

    数据库模型及迁移

--- resources/views/gii/create.blade.php
+++ resources/views/gii/create.blade.php
@@ -25,6 +25,11 @@
     .ivu-back-top i {
       padding: 4px;
     }
+
+    hr {
+      border-top: 1px dashed #eee;
+      margin: .5rem 0;
+    }
   </style>
 </head>
 <body>
@@ -32,89 +37,103 @@
     <h1 class="text-center my-5">
       Laravel Gii
     </h1>
-    <div>
-      <div class="container">
-        <i-form ref="formValidate" :model="formValidate" :rules="ruleValidate" label-position="top">
-          <div class="row">
-            <div class="col">
-              <form-item label="模块名">
-                <i-input v-model="formValidate.module" placeholder="可不填" clearable></i-input>
-              </form-item>
-            </div>
-            <div class="col">
-              <form-item label="表名" prop="table">
-                <i-input v-model="formValidate.table" placeholder="请输入表名" clearable></i-input>
-              </form-item>
-            </div>
-          </div>
-          <div>
-            <div class="row align-items-center" v-for="(v, k) in formValidate.columns" :key="k">
-              <div class="col">
-                <form-item
-                  label="字段名"
-                  :prop="`columns.${k}.name`"
-                  :rules="{required: true, message: '请输入字段名', trigger: 'change'}"
-                >
-                  <i-input v-model="v.name" placeholder="请输入字段名" clearable></i-input>
-                </form-item>
-              </div>
-              <div class="col">
-                <form-item
-                  label="类型"
-                  :prop="`columns.${k}.type`"
-                  :rules="{required: true, message: '请选择类型', trigger: 'change'}"
-                >
-                  <i-select placeholder="请选择类型" v-model="v.type">
-                    <i-option :value="type" v-for="type in columnTypes" :key="type">
-                      @{{ type }}
-                    </i-option>
-                  </i-select>
-                </form-item>
-              </div>
-              <div class="col-auto">
-                <form-item label="可空">
-                  <i-switch size="large" v-model="v.nullable">
-                    <span slot="open">可空</span>
-                    <span slot="close">非空</span>
-                  </i-switch>
-                </form-item>
-              </div>
-              <div class="col">
-                <form-item label="默认">
-                  <i-input v-model="v.default" placeholder="可不填" clearable></i-input>
-                </form-item>
-              </div>
-              <div class="col">
-                <form-item label="注释">
-                  <i-input v-model="v.comment" placeholder="可不填" clearable></i-input>
-                </form-item>
-              </div>
-              <div class="col-auto">
-                <form-item label="操作">
-                  <poptip
-                    confirm
-                    title="确认删除？"
-                    @on-ok="delColumn(k)">
-                    <i-button type="error">
-                      删除
-                    </i-button>
-                  </poptip>
-                </form-item>
-              </div>
-            </div>
-            <form-item>
-              <i-button type="dashed" long @click="addColumn">
-                新增字段
-              </i-button>
+    <div class="container">
+      <i-form ref="formValidate" :model="formValidate" :rules="ruleValidate" label-position="top">
+        <div class="row">
+          <div class="col">
+            <form-item label="模块名">
+              <i-input v-model="formValidate.module" placeholder="可不填" clearable></i-input>
             </form-item>
           </div>
+          <div class="col">
+            <form-item label="表名" prop="table">
+              <i-input v-model="formValidate.table" placeholder="请输入表名" clearable></i-input>
+            </form-item>
+          </div>
+        </div>
+        <div>
+          <div class="row align-items-center" v-for="(v, k) in formValidate.columns" :key="k">
+            <div class="col">
+              <form-item
+                label="字段名"
+                :prop="`columns.${k}.name`"
+                :rules="{required: true, message: '请输入字段名', trigger: 'change'}"
+              >
+                <i-input v-model="v.name" placeholder="请输入字段名" clearable></i-input>
+              </form-item>
+            </div>
+            <div class="col">
+              <form-item
+                label="类型"
+                :prop="`columns.${k}.type`"
+                :rules="{required: true, message: '请选择类型', trigger: 'change'}"
+              >
+                <i-select placeholder="请选择类型" v-model="v.type">
+                  <i-option :value="type" v-for="type in columnTypes" :key="type">
+                    @{{ type }}
+                  </i-option>
+                </i-select>
+              </form-item>
+            </div>
+            <div class="col-auto">
+              <form-item label="唯一">
+                <i-switch size="large" v-model="v.unique">
+                  <span slot="open">是</span>
+                  <span slot="close">否</span>
+                </i-switch>
+              </form-item>
+            </div>
+            <div class="col-auto">
+              <form-item label="可空">
+                <i-switch size="large" v-model="v.nullable">
+                  <span slot="open">是</span>
+                  <span slot="close">否</span>
+                </i-switch>
+              </form-item>
+            </div>
+            <div class="col-auto">
+              <form-item label="可写">
+                <i-switch size="large" v-model="v.writable">
+                  <span slot="open">是</span>
+                  <span slot="close">否</span>
+                </i-switch>
+              </form-item>
+            </div>
+            <div class="col">
+              <form-item label="默认">
+                <i-input v-model="v.default" placeholder="可不填" clearable></i-input>
+              </form-item>
+            </div>
+            <div class="col">
+              <form-item label="注释">
+                <i-input v-model="v.comment" placeholder="可不填" clearable></i-input>
+              </form-item>
+            </div>
+            <div class="col-auto">
+              <form-item label="操作">
+                <poptip
+                  confirm
+                  title="确认删除？"
+                  @on-ok="delColumn(k)">
+                  <i-button type="error">
+                    删除
+                  </i-button>
+                </poptip>
+              </form-item>
+            </div>
+          </div>
           <form-item>
-            <i-button type="primary" long @click="handleSubmit" :loading="loading">
-              提交
+            <i-button type="dashed" long @click="addColumn">
+              新增字段
             </i-button>
           </form-item>
-        </i-form>
-      </div>
+        </div>
+        <form-item>
+          <i-button type="primary" long @click="handleSubmit" :loading="loading">
+            提交
+          </i-button>
+        </form-item>
+      </i-form>
     </div>
     <div>
       <back-top :bottom="8" :right="8"/>
@@ -122,6 +141,7 @@
   </div>
   <script src="https://cdn.bootcss.com/vue/2.6.11/vue.min.js"></script>
   <script src="https://cdn.bootcss.com/iview/3.5.4/iview.min.js"></script>
+  <script src="https://cdn.bootcss.com/axios/0.19.2/axios.min.js"></script>
   <script>
     const app = new Vue({
       el: '#app',
@@ -194,13 +214,28 @@
         ]
       },
       mounted() {
+        axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+        axios.defaults.headers.common['X-CSRF-TOKEN'] = document.head.querySelector('meta[name="csrf-token"]').content;
+
         this.addColumn();
       },
       methods: {
         handleSubmit() {
           this.$refs.formValidate.validate((valid) => {
             if (valid) {
+              this.loading = true;
 
+              axios
+                .post('/gii', this.formValidate)
+                .then(({data}) => {
+
+                  this.loading = false;
+                })
+                .catch(({response}) => {
+                  this.$Message.error(response.data.message);
+
+                  this.loading = false;
+                });
             }
           });
         },
@@ -208,7 +243,9 @@
           return {
             type: this.columnTypes[0],
             name: '',
+            unique: false,
             nullable: false,
+            writable: true,
             default: '',
             comment: ''
           };
--- src/Http/Controllers/GiiController.php
+++ src/Http/Controllers/GiiController.php
@@ -2,6 +2,7 @@
 
 namespace Orh\LaravelGii\Http\Controllers;
 
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Http\Request;
 
 class GiiController
@@ -11,8 +12,103 @@ class GiiController
         return view('gii::gii.create');
     }
 
-    public function store(Request $request)
+    public function store(Request $request, Filesystem $file)
     {
+        $module = $request->input('module');
+        $table = $request->input('table');
+        $columns = $request->input('columns');
 
+        $this->buildModel($table, $columns);
+        $this->buildMigration($table, $columns);
+
+        return;
+    }
+
+    protected function buildModel($table, $columns)
+    {
+        $dummyClass = ucfirst(str_singular($table));
+
+        $path = "app/Models/{$dummyClass}.php";
+
+        $columns = collect($columns)
+            ->filter(function ($v) {
+                return $v['writable'];
+            })
+            ->pluck('name')
+            ->map(function ($v) {
+                return "'{$v}',";
+            })
+            ->implode("\n        ");
+
+        $search = [
+            'DummyClass',
+            'DummyColumns',
+        ];
+
+        $replace = [
+            $dummyClass,
+            $columns,
+        ];
+
+        $this->createStub('model', $path, $search, $replace);
+
+        return $path;
+    }
+
+    protected function buildMigration($table, $columns)
+    {
+        $timestamp = date('Y_m_d_His');
+        $filename = "create_{$table}_table";
+
+        $path = "database/migrations/{$timestamp}_{$filename}.php";
+
+        $columns = collect($columns)
+            ->map(function ($v) {
+                $tmp = '$table->' . "{$v['type']}('{$v['name']}')";
+
+                $tmp .= $v['unique'] ? '->unique()' : '';
+
+                $tmp .= $v['nullable'] ? '->nullable()' : '';
+
+                $tmp .= $v['default'] ? "->default('{$v['default']}')" : '';
+
+                $tmp .= $v['comment'] ? "->comment('{$v['comment']}')" : '';
+
+                $tmp .= ';';
+
+                return $tmp;
+            })
+            ->implode("\n            ");
+
+        $search = [
+            'DummyClass',
+            'DummyTable',
+            'DummyColumns',
+        ];
+
+        $replace = [
+            studly_case($filename),
+            $table,
+            $columns,
+        ];
+
+        $this->createStub('migration', $path, $search, $replace);
+
+        return $path;
+    }
+
+    protected function createStub($type, $path, $search, $replace)
+    {
+        $file = app(Filesystem::class);
+
+        if ($file->exists($path = base_path($path))) {
+            return;
+        }
+
+        $stub = $file->get(__DIR__ . "/../../../stubs/{$type}.stub");
+
+        $stub = str_replace($search, $replace, $stub);
+
+        $file->put($path, $stub);
     }
 }
