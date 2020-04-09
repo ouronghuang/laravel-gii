4f44243e5a1b1fd5d5fb0fa48046378f928b107f
欧荣凰 <512574561@qq.com>
2020-04-09 01:45:17 +0000

    创建控制器

--- src/Http/Controllers/GiiController.php
+++ src/Http/Controllers/GiiController.php
@@ -14,12 +14,18 @@ class GiiController
 
     public function store(Request $request, Filesystem $file)
     {
-        $module = $request->input('module');
-        $table = $request->input('table');
-        $columns = $request->input('columns');
+        $module = strtolower($request->input('module'));
+        $table = strtolower($request->input('table'));
+        $columns = collect($request->input('columns'))
+            ->map(function ($v) {
+                $v['name'] = strtolower($v['name']);
+
+                return $v;
+            });
 
         $this->buildModel($table, $columns);
-        $this->buildMigration($table, $columns);
+        // $this->buildMigration($table, $columns);
+        $this->buildController($module, $table, $columns);
 
         return;
     }
@@ -97,6 +103,51 @@ class GiiController
         return $path;
     }
 
+    protected function buildController($module, $table, $columns)
+    {
+        $dummyModel = studly_case(str_singular($table));
+
+        $dummyClass = str_plural($dummyModel) . 'Controller';
+
+        $path = 'app/Http/Controllers/' . ($module ? ucfirst($module) . '/' : '') . "{$dummyClass}.php";
+
+        $columns = collect($columns)
+            ->filter(function ($v) {
+                return $v['writable'];
+            })
+            ->pluck('name')
+            ->map(function ($v) {
+                return "'{$v}',";
+            })
+            ->implode("\n            ");
+
+        $search = [
+            'DummyModule',
+            'DummyRequest',
+            'DummyResource',
+            'DummyModel',
+            'dummyModel',
+            'DummyClass',
+            'DummyView',
+            'DummyColumns',
+        ];
+
+        $replace = [
+            $module ? '\\' . ucfirst($module) : '',
+            "{$dummyModel}Request",
+            "{$dummyModel}Resource",
+            $dummyModel,
+            camel_case($dummyModel),
+            $dummyClass,
+            ($module ? "{$module}." : '') . "{$table}",
+            $columns,
+        ];
+
+        $this->createStub('controller', $path, $search, $replace);
+
+        return $path;
+    }
+
     protected function createStub($type, $path, $search, $replace)
     {
         $file = app(Filesystem::class);
@@ -105,10 +156,25 @@ class GiiController
             return;
         }
 
+        $this->mkdir($path);
+
         $stub = $file->get(__DIR__ . "/../../../stubs/{$type}.stub");
 
         $stub = str_replace($search, $replace, $stub);
 
         $file->put($path, $stub);
+    }
+
+    protected function mkdir($path)
+    {
+        $directory = dirname($path);
+
+        if (!is_dir($directory) && (false === @mkdir($directory, 0777, true)) && !is_dir($directory)) {
+            return false;
+        } elseif (!is_writable($directory)) {
+            return false;
+        }
+
+        return true;
     }
 }
